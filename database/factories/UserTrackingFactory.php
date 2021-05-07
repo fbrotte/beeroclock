a032f0983e8ad20cc40307f669b3596aececd0fe
Yoan savary <ysys9696@live.fr>
2021-05-07 11:51:37 +0200

    feat(structure): make laravel file structure

---
+++ database/factories/UserTrackingFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\UserTracking;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class UserTrackingFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = UserTracking::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            //
+        ];
+    }
+}
