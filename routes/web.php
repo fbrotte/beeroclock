993badc4077094e016b3dd3ce3dcb9bc0fc50ee7
Yoan savary <ysys9696@live.fr>
2021-05-07 16:42:09 +0200

    feat(auth): breeze install

--- routes/web.php
+++ routes/web.php
@@ -13,4 +13,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::view('/', 'pages.vitrine.home')->name('index');
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+require __DIR__.'/auth.php';
