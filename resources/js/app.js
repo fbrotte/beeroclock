4c5beec2e7bc5cc631926970fcaca06669b34f56
Yoan savary <ysys9696@live.fr>
2021-05-10 10:10:31 +0200

    feat(view): add slider

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,3 +1,9 @@
 require('./bootstrap');
 
 require('alpinejs');
+  // import Swiper JS
+  import Swiper from 'swiper';
+  // import Swiper styles
+  import 'swiper/swiper-bundle.css';
+
+  const swiper = new Swiper(".slider");
--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ use App\Http\Controllers\UserController;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('pages.vitrine.home');
 });
 
 Route::get('admin', [UserController::class, 'user_list']);
