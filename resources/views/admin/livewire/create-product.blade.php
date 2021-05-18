03ca64a67bbdf3659144ad94696eccf7fa28d34b
Yoan savary <ysys9696@live.fr>
2021-05-18 11:53:24 +0200

    fix(dashboard) reset when user unselect choice

--- resources/views/admin/livewire/create-product.blade.php
+++ resources/views/admin/livewire/create-product.blade.php
@@ -1,20 +1,18 @@
 <form class="mt-6" wire:submit.prevent="submit" x-data="formController()" x-cloak>
 
     @if (session()->has('message'))
-    <div class="alert alert-success">
-        {{ session('message') }}
-    </div>
+        <div class="alert alert-success">
+            {{ session('message') }}
+        </div>
     @endif
 
-    <h3 x-text="choice"> </h3>
-    
     <label class="block mt-2 text-xs font-semibold text-gray-600 uppercase">Catégorie</label>
     @error('product_type_id') <span class="error">{{ $message }}</span> @enderror
     <select x-model.number="choice"
         class="border border-gray-300 text-gray-600 h-10 pl-5 pr-10 bg-white hover:border-gray-400 focus:outline-none appearance-none"
         wire:model.defer="product_type_id">
 
-        <option value="" selected>Choisis une categorie</option>
+        <option value="0" selected>Choisis une categorie</option>
         @foreach($productType as $item)
         <option :value="{{ $item->id }}">{{ $item->name }}</option>
         @endforeach
@@ -29,12 +27,10 @@
         <x-forms.create-product.input type="number" step="0.01" id="price">Prix</x-forms.create-product.input>
     </div>
 
-
     <button type="submit"
         class="w-full py-3 mt-6 font-medium tracking-widest text-white uppercase bg-black shadow-lg focus:outline-none hover:bg-gray-900 hover:shadow-none">
         créer
     </button>
-
 </form>
 
 
@@ -69,13 +65,6 @@ function formController() {
         },
         qty: {},
         price: {},
-
-
-        // alcohol: {
-        //     ['x-show']() {
-        //         return alcohol.includes(this->choice)
-        //     },
-        // },
     }
 }
     
